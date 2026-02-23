b45fc39355214532f1ec7b11000f29962e87a026
sriram32134 <jnanasriramubbisetti@gmail.com>
2026-02-23 12:47:34 +0530

    Replace frontend2 completely with new frontend folder

---
+++ client/src/data/dummyArticle.js
@@ -0,0 +1,31 @@
+const dummyData = [
+  {
+    id: 1,
+    title: "AI is Transforming the World",
+    category: "Technology",
+    tags: ["ai", "ml", "future"],
+    likes: 120,
+    views: 500,
+    createdAt: "2026-02-15"
+  },
+{
+  id: 2,
+  title: "Education Reform 2026",
+  category: "Education",
+  tags: ["policy", "students"],
+  likes: 2000,
+  views: 5000,
+  createdAt: "2026-02-10"
+},
+
+  {
+    id: 3,
+    title: "Latest Sports Highlights",
+    category: "Sports",
+    tags: ["football", "cricket"],
+    likes: 200,
+    views: 800,
+    createdAt: "2026-02-17"
+  }
+]
+export default dummyData;
