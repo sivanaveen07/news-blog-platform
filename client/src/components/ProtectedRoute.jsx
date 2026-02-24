5dc80d982afaa6710a6cfeaa2dbe637e60376b20
sriram32134 <jnanasriramubbisetti@gmail.com>
2026-02-24 20:43:58 +0530

    Clean frontend2 branch

--- client/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-function ProtectedRoutes({ isLoggedIn, children }) {
-    if (!isLoggedIn) {
-        return <Navigate to="/login" />;
-    }
-    return children;
-}
-export default ProtectedRoutes;
--- server/routes/articleRoutes.js
+++
@@ -1,9 +0,0 @@
-const {createArticle,getArticles,getArticleById}= require('../controllers/articleController')
-const express = require('express')
-const router = express.Router() 
-
-router.post('/articles',createArticle)
-router.get('/articles',getArticles)
-router.get('/articles/:id',getArticleById)
-
-module.exports = router
